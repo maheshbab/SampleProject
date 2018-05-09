3cbce238581efbf9995f54f19fac1289fb15d257
mahesh.kalamoori <mahesh.kalamoori@thomsonreuters.com>
2018-05-09 22:46:56 +0530

    i have chnaged some lines

--- src/com/app/Address.java
+++ src/com/app/Address.java
@@ -3,8 +3,21 @@ package com.app;
 public class Address {
 	
 	private int addrId;
+
+	public int getAddrId() {
+		return addrId;
+	}
+
+	public void setAddrId(int addrId) {
+		this.addrId = addrId;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + "]";
+	}
 	
-	private String addrloc;
+	
 	
 	
 	
