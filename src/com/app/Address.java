70a6db1a5096fa5417f9e75ce1fdab7e89046199
mahesh.kalamoori <mahesh.kalamoori@thomsonreuters.com>
2018-05-09 22:51:02 +0530

    commit the address

--- src/com/app/Address.java
+++ src/com/app/Address.java
@@ -3,6 +3,12 @@ package com.app;
 public class Address {
 	
 	private int addrId;
+   
+	private String pincode;
+	
+	private int mobileNo;
+	
+	private String emaill;
 
 	public int getAddrId() {
 		return addrId;
@@ -12,13 +18,35 @@ public class Address {
 		this.addrId = addrId;
 	}
 
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+	public int getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmaill() {
+		return emaill;
+	}
+
+	public void setEmaill(String emaill) {
+		this.emaill = emaill;
+	}
+
 	@Override
 	public String toString() {
-		return "Address [addrId=" + addrId + "]";
+		return "Address [addrId=" + addrId + ", pincode=" + pincode + ", mobileNo=" + mobileNo + ", emaill=" + emaill
+				+ "]";
 	}
-	
-	
-	
 	
 	
 
