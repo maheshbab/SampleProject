c70b59b1e8770ceee8c902ed3529f8da4fcd6018
maheshbab <39083378+maheshbab@users.noreply.github.com>
2018-05-10 22:34:01 +0530

    Update Address.java

--- src/com/app/Address.java
+++ src/com/app/Address.java
@@ -10,39 +10,4 @@ public class Address {
 	
 	private String emaill;
 
-	public int getAddrId() {
-		return addrId;
-	}
-
-	public void setAddrId(int addrId) {
-		this.addrId = addrId;
-	}
-
-	public String getPincode() {
-		return pincode;
-	}
-
-	public void setPincode(String pincode) {
-		this.pincode = pincode;
-	}
-
-	public int getMobileNo() {
-		return mobileNo;
-	}
-
-	public void setMobileNo(int mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-
-	public String getEmaill() {
-		return emaill;
-	}
-
-	public void setEmaill(String emaill) {
-		this.emaill = emaill;
-	}
-
-	
-	
-
 }
